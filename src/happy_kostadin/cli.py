8c290394f732b603ac5e0e2ff8f535955216614e
wagenrace <nijhof.dns@gmail.com>
2025-03-27 15:49:53 +0100

    switch to using config

--- src/happy_kostadin/cli.py
+++ src/happy_kostadin/cli.py
@@ -1,66 +1,23 @@
-import argparse
 import os
 import sys
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Dict, Union
+from typing import Union
 
 from tqdm import tqdm
 
-if sys.version_info >= (3, 11):
-    try:
-        import tomllib
-    except ImportError:
-        # Help users on older alphas
-        if not TYPE_CHECKING:
-            import tomli as tomllib
-else:
-    import tomli as tomllib
+from .get_config.get_config import get_config, Config
 
-
-def parse_pyproject_toml() -> Dict[str, Any]:
-    """Parse a pyproject toml file, pulling out relevant parts for Black.
-
-    If parsing fails, will raise a tomllib.TOMLDecodeError.
-    """
-    path_pyproject_toml = Path.cwd() / "pyproject.toml"
-    if not path_pyproject_toml.exists():
-        return {}
-    with open(path_pyproject_toml, "rb") as f:
-        pyproject_toml = tomllib.load(f)
-    config: Dict[str, Any] = pyproject_toml.get("tool", {}).get("happy_kostadin", {})
-    config = {k.replace("--", "").replace("-", "_"): v for k, v in config.items()}
-
-    return config
-
-
-def __get_arguments() -> Path:
-    parser = argparse.ArgumentParser(
-        description="A happy Kostadin is a good Kostadin",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument(
-        "dominant_path",
-        type=str,
-        help="Get the path to the files you want to check for line endings",
-        nargs="?",
-        default="",
-    )
-    parser.add_argument(
-        "-p",
-        "--path",
-        type=str,
-        default="",
-    )
-    args = parser.parse_args()
-    config = vars(args)
-    if config.get("dominant_path"):
-        path = Path(config["dominant_path"]).absolute()
-    elif config.get("path"):
-        path = Path(config["path"]).absolute()
-    else:
-        path = Path.cwd().absolute()
-    return path
-
+def get_all_files(config: Config) -> list:
+    all_files = []
+    allowed_post_fixes = tuple(config.allowed_post_fixes)
+    for root, _, files in os.walk(config.path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if len(allowed_post_fixes) > 0:
+                if not file_path.endswith(allowed_post_fixes):
+                    continue
+            all_files.append(Path(file_path))
+    return all_files
 
 def main(return_checked_files: bool = False) -> Union[list, None]:
     """Will raise a ValueError if any of the files contain CRLF line endings.
@@ -71,26 +28,17 @@ def main(return_checked_files: bool = False) -> Union[list, None]:
     :return: all the files that are checked or None
     :rtype: Union[list, None]
     """
-    path = __get_arguments()
-    config = parse_pyproject_toml()
-    allowed_post_fixes = tuple(config.get("allowed_post_fixes", []))
-    print(f"checking for CRLF in {path}")
+    config = get_config()
+    print(f"checking for CRLF in {config.path}")
 
-    all_files = []
-    for root, _, files in os.walk(path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            if len(allowed_post_fixes) > 0:
-                if not file_path.endswith(allowed_post_fixes):
-                    continue
-            all_files.append(Path(file_path))
+    all_files = get_all_files(config)
 
     # Usage example
     files_containing_crlf = []
     for file in tqdm(all_files):
         content = open(file, "rb").read()
         if b"\r\n" in content:
-            file_name = str(file).replace(str(path), "")
+            file_name = str(file).replace(str(config.path), "")
             files_containing_crlf.append(file_name)
 
     if files_containing_crlf:
